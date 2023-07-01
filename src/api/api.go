30dcbdfe5010ee412e556bb176d83fa03a3e2164
dominic-wassef <domwassef@gmail.com>
2023-07-01 03:26:09 -0400

    adding three repo methods to api

--- src/api/api.go
+++ src/api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/dominic-wassef/golander/src/database"
 	"github.com/gin-gonic/gin"
@@ -25,10 +26,53 @@ func (s *Server) Start(port string) {
 
 func (s *Server) routes() {
 	s.router.GET("/ping", s.pingHandler)
+	s.router.GET("/repositories", s.getRepositoriesHandler)
+	s.router.GET("/repository/:id", s.getRepositoryByIDHandler)
+	s.router.DELETE("/repository/:id", s.deleteRepositoryHandler)
 }
 
 func (s *Server) pingHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
+}
+
+// Handler for getting all repositories
+func (s *Server) getRepositoriesHandler(c *gin.Context) {
+	repositories, err := s.db.GetAllRepos()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, repositories)
+}
+
+// Handler for getting a repository by ID
+func (s *Server) getRepositoryByIDHandler(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid parameter"})
+		return
+	}
+	repo, err := s.db.GetRepoByID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, repo)
+}
+
+// Handler for deleting a repository
+func (s *Server) deleteRepositoryHandler(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid parameter"})
+		return
+	}
+	err = s.db.DeleteRepoByID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"status": "deleted"})
 }
