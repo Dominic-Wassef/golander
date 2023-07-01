e7f95096eae530b9db066c93837712a41811d827
dominic-wassef <domwassef@gmail.com>
2023-07-01 03:28:25 -0400

    adding defaults and validate to config

--- src/config/config.go
+++ src/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"errors"
+	"net/url"
 	"os"
 	"strconv"
 )
@@ -28,37 +30,59 @@ type ScraperConfig struct {
 }
 
 func Load() (*Config, error) {
-	portStr := os.Getenv("API_PORT")
-	if portStr == "" {
-		portStr = "8080" // default value
-	}
+	// Set defaults
+	portStr := getEnv("API_PORT", "8080")
+	dbportStr := getEnv("DB_PORT", "3306")
+	host := getEnv("DB_HOST", "localhost")
+	user := getEnv("DB_USER", "root")
+	password := getEnv("DB_PASSWORD", "")
+	dbname := getEnv("DB_NAME", "golander")
+	scraperURL := getEnv("SCRAPER_URL", "https://github.com/trending")
+
+	// Parse integers
 	port, err := strconv.Atoi(portStr)
 	if err != nil {
 		return nil, err
-	}
-
-	dbportStr := os.Getenv("DB_PORT") // Use "DB_PORT" not "API_PORT"
-	if dbportStr == "" {
-		dbportStr = "3306" // default value
 	}
 	dbPort, err := strconv.Atoi(dbportStr)
 	if err != nil {
 		return nil, err
 	}
 
+	// Validate
+	if port < 1 || port > 65535 {
+		return nil, errors.New("invalid API port")
+	}
+	if dbPort < 1 || dbPort > 65535 {
+		return nil, errors.New("invalid database port")
+	}
+	_, err = url.Parse(scraperURL)
+	if err != nil {
+		return nil, errors.New("invalid scraper URL")
+	}
+
 	return &Config{
 		Database: DatabaseConfig{
-			Host:     os.Getenv("DB_HOST"),
+			Host:     host,
 			Port:     dbPort,
-			User:     os.Getenv("DB_USER"),
-			Password: os.Getenv("DB_PASSWORD"),
-			DBName:   os.Getenv("DB_NAME"),
+			User:     user,
+			Password: password,
+			DBName:   dbname,
 		},
 		API: APIConfig{
 			Port: port,
 		},
 		Scraper: ScraperConfig{
-			URL: os.Getenv("SCRAPER_URL"),
+			URL: scraperURL,
 		},
 	}, nil
+}
+
+// getEnv gets an environment variable or returns a default value
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		value = defaultValue
+	}
+	return value
 }
