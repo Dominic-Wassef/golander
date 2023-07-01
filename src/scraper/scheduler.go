8d98e64ab6608075aff97fd3da7c9eaf97c03892
dominic-wassef <domwassef@gmail.com>
2023-07-01 03:28:52 -0400

    chore: scheduler.go

--- src/scraper/scheduler.go
+++ src/scraper/scheduler.go
@@ -2,18 +2,91 @@ package scraper
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-// New starts a new scheduler that runs the scraping at regular intervals
-func New(scrapingInterval time.Duration, scrapeFunc func()) {
-	ticker := time.NewTicker(scrapingInterval)
+type TaskFunc func() error
+
+type Task struct {
+	Interval     time.Duration
+	LastRun      time.Time
+	Running      bool
+	stop         chan bool
+	taskFunc     TaskFunc
+	errorHandler func(error)
+}
+
+func NewTask(interval time.Duration, taskFunc TaskFunc, errorHandler func(error)) *Task {
+	return &Task{
+		Interval:     interval,
+		taskFunc:     taskFunc,
+		errorHandler: errorHandler,
+		stop:         make(chan bool),
+	}
+}
+
+func (t *Task) Start() {
+	t.Running = true
 
 	go func() {
-		for range ticker.C {
-			// Call web scraping function here
-			fmt.Println("Starting the scraping task...")
-			scrapeFunc()
+		ticker := time.NewTicker(t.Interval)
+
+		for {
+			select {
+			case <-ticker.C:
+				t.LastRun = time.Now()
+				fmt.Printf("Starting task at %v\n", t.LastRun)
+				if err := t.taskFunc(); err != nil {
+					t.errorHandler(err)
+				}
+			case <-t.stop:
+				ticker.Stop()
+				return
+			}
 		}
 	}()
+}
+
+func (t *Task) Stop() {
+	t.Running = false
+	t.stop <- true
+}
+
+type Scheduler struct {
+	tasks []*Task
+	mutex sync.Mutex
+}
+
+func NewScheduler() *Scheduler {
+	return &Scheduler{}
+}
+
+func (s *Scheduler) AddTask(t *Task) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.tasks = append(s.tasks, t)
+}
+
+func (s *Scheduler) StartAll() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	for _, task := range s.tasks {
+		if !task.Running {
+			task.Start()
+		}
+	}
+}
+
+func (s *Scheduler) StopAll() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	for _, task := range s.tasks {
+		if task.Running {
+			task.Stop()
+		}
+	}
 }
